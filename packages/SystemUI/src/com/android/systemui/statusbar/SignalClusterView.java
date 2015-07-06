5afeba77b606e299e707f602f1be8320a339e30a
George G <kreach3r@users.noreply.github.com>
2015-07-06 23:13:37 +0300

    SystemUI: FIX No sim - airplane mode padding
    Steps to reproduce:
    Enable airplane mode
    Reboot
    
    Check the statusbar. The no sim icon is too close to airplane icon. This commit shows mWifiAirplaneSpacer everytime airplane mode is on, and not only when wifi is also on, and fixes that.

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -522,7 +522,7 @@ public class SignalClusterView
             mImsAirplane.setVisibility(View.GONE);
         }
 
-        if (mIsAirplaneMode && mWifiVisible) {
+        if (mIsAirplaneMode) {
             mWifiAirplaneSpacer.setVisibility(View.VISIBLE);
         } else {
             mWifiAirplaneSpacer.setVisibility(View.GONE);
